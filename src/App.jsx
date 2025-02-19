5c0ccf2e3525ce90e6fc26327d3e931c1630ce2d
parash <parashmusic770@gmail.com>
2025-02-20 02:10:27 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { Header } from "./components/Header"
-import { HeroSection } from "./components/HeroSection"
-import { FeaturedCategories } from "./components/FeaturedCategories"
-import { ProductGrid } from "./components/ProductGrid"
-import { PromoBar } from "./components/PromoBar"
-import { Routes,Route } from "react-router-dom"
-import { ProductDetailPage } from './components/Product';
-import Home from "./pages/Home"
-import Footer from "./components/ui/Footer"
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div >
-      {/*
-      <main>
-        <HeroSection />
-        <FeaturedCategories />
-        <ProductGrid />
-      </main> */}
-       <Header />
-      
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/product/:id" element={<ProductDetailPage />} />
-      </Routes>
-      <Footer/>
-    </div>
+    <div>App</div>
   )
 }
 
 export default App
-
